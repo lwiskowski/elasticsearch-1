2cd4f14300ee08f57c51ff4e0a2de63d2654aa6c
lwiskowski2014 <lwiskowski@Lukes-MacBook-Pro.local>
2016-05-14 00:36:51 -0400

    new es

--- NewTestSuiteForAbstractQuery.java
+++
@@ -1,8 +0,0 @@
-import org.apache.lucene.search.PrefixQuery;
-
-protected static void assertPrefixQuery(Query query, String field, String value) {
-    assertThat(query, instanceOf(PrefixQuery.class));
-    PrefixQuery prefixQuery = (PrefixQuery) query;
-    assertThat(prefixQuery.getPrefix().text(), equalTo(value));
-    assertThat(prefixQuery.getPrefix().field(), equalTo(field));    
-}
--- testElastic4.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/sh
-
-curl localhost:9200/_cat/indices
-curl http://localhost:9200/_cat/indices/\*_results-\*
